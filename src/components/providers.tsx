d5852e22f68ee9276f67482a8df532570b6d63d2
pzinwai <1564107+pzinwai@users.noreply.github.com>
2025-03-17 17:23:30 -0700

    Update header, footer and intro.

---
+++ src/components/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ThemeProvider, useTheme } from 'next-themes'
+export default function Providers({ children }: {children: React.ReactNode}) {
+  return (
+    <ThemeProvider
+      enableSystem
+      attribute='class'
+      defaultTheme='light'
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  )
+}
